831d04f9d3967b51ecd16b0370c4d1bb83dca11b
TELECOM\u633396 <EMaldonado2@teco.com.ar>
2023-06-16 14:30:12 -0300

    Login del user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -27,12 +28,30 @@ export class AuthService {
 
       await this.userRepository.save(user);
       delete user.password;
-      
+
       return user;
 
     } catch (error) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login ( loginUserDto: LoginUserDto ) {
+      const { email, password } = loginUserDto;
+
+      const user = await this.userRepository.findOne({ 
+        where: { email },
+        select: { email: true, password: true }
+       });
+
+      if (!user) {
+        throw new UnauthorizedException('Invalid credentials (email)');
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) { //COMPARA LA PASSWORD QUE INGRESA EL USER CON LA QUE ESTA EN LA BASE DE DATOS
+        throw new UnauthorizedException('Invalid credentials (password)');
+      }
+      return user;
   }
 
   private handleDBErrors( error: any) {
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -10,7 +10,7 @@ export class User {
     @Column({ unique: true })
     email: string;
 
-    @Column('text')
+    @Column('text', { select: false })
     password: string;
 
     @Column('text')
